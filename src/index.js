ac4c625d1967b8fa53c74ceeeb129b3e4168b319
Kostya <kostyapenyavskiy@gmail.com>
2021-04-30 11:47:54 +0300

    solution

---
+++ src/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-magic-numbers */
+
+function findPalindrome(num) {
+    const reversedNum = +num.toString().split('').reverse().join('');
+    findPalindrome.steps = findPalindrome.steps ?? 0;
+
+    if (num === reversedNum) {
+        return {
+            num,
+            steps: findPalindrome.steps || 1,
+        };
+    }
+
+    findPalindrome.steps++;
+
+    try {
+        return findPalindrome(num + reversedNum);
+    } catch (e) {
+        throw new Error(`${num} - is Lycher number.`);
+    }
+}
